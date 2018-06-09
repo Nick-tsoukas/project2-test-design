a9263fb6fbd61acf11c4637150877154962c94ef
Nikolaos Tsoukas <nikolaostsoukas@Nikolaoss-iMac.local>
2018-06-09 12:57:04 -0600

    Changing styles

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 // Instantiate `CircleType` with an HTML element.
-// const circleType = new CircleType(document.getElementById('demo1'));
-//
-// // Set the text radius and direction. Note: setter methods are chainable.
-// circleType.radius(384);
+const circleType = new CircleType(document.getElementById('demo1'));
 
-// var demo1 = new CircleType(document.getElementById('demo1'));
-// window.addEventListener('resize', function updateRadius() {
-//   demo1.radius(demo1.element.offsetWidth / 2);
-// });
-// updateRadius();
+// Set the text radius and direction. Note: setter methods are chainable.
+circleType.radius(384);
+
+var demo2 = new CircleType(document.getElementById('demo2'));
+
+demo2.radius(384);
 
 // Provide your own splitter function to handle emojis
 // @see https://github.com/orling/grapheme-splitter
@@ -19,105 +17,4 @@
 // );
 
 
-var penner = anime.timeline();
-penner
-  .add({
-    targets: '#penner .linear', translateX: 250, offset: 0,
-    easing: 'linear'
-  })
-  .add({
-    targets: '#penner .InQuad', translateX: 250, offset: 0,
-    easing: 'easeInQuad'
-  })
-  .add({
-    targets: '#penner .InCubic', translateX: 250, offset: 0,
-    easing: 'easeInCubic'
-  })
-  .add({
-    targets: '#penner .InQuart', translateX: 250, offset: 0,
-    easing: 'easeInQuart'
-  })
-  .add({
-    targets: '#penner .InQuint', translateX: 250, offset: 0,
-    easing: 'easeInQuint'
-  })
-  .add({
-    targets: '#penner .InSine', translateX: 250, offset: 0,
-    easing: 'easeInSine'
-  })
-  .add({
-    targets: '#penner .InExpo', translateX: 250, offset: 0,
-    easing: 'easeInExpo'
-  })
-  .add({
-    targets: '#penner .InCirc', translateX: 250, offset: 0,
-    easing: 'easeInCirc'
-  })
-  .add({
-    targets: '#penner .InBack', translateX: 250, offset: 0,
-    easing: 'easeInBack'
-  })
-  .add({
-    targets: '#penner .OutQuad', translateX: 250, offset: 0,
-    easing: 'easeOutQuad'
-  })
-  .add({
-    targets: '#penner .OutCubic', translateX: 250, offset: 0,
-    easing: 'easeOutCubic'
-  })
-  .add({
-    targets: '#penner .OutQuart', translateX: 250, offset: 0,
-    easing: 'easeOutQuart'
-  })
-  .add({
-    targets: '#penner .OutQuint', translateX: 250, offset: 0,
-    easing: 'easeOutQuint'
-  })
-  .add({
-    targets: '#penner .OutSine', translateX: 250, offset: 0,
-    easing: 'easeOutSine'
-  })
-  .add({
-    targets: '#penner .OutExpo', translateX: 250, offset: 0,
-    easing: 'easeOutExpo'
-  })
-  .add({
-    targets: '#penner .OutCirc', translateX: 250, offset: 0,
-    easing: 'easeOutCirc'
-  })
-  .add({
-    targets: '#penner .OutBack', translateX: 250, offset: 0,
-    easing: 'easeOutBack'
-  })
-  .add({
-    targets: '#penner .InOutQuad', translateX: 250, offset: 0,
-    easing: 'easeInOutQuad'
-  })
-  .add({
-    targets: '#penner .InOutCubic', translateX: 250, offset: 0,
-    easing: 'easeInOutCubic'
-  })
-  .add({
-    targets: '#penner .InOutQuart', translateX: 250, offset: 0,
-    easing: 'easeInOutQuart'
-  })
-  .add({
-    targets: '#penner .InOutQuint', translateX: 250, offset: 0,
-    easing: 'easeInOutQuint'
-  })
-  .add({
-    targets: '#penner .InOutSine', translateX: 250, offset: 0,
-    easing: 'easeInOutSine'
-  })
-  .add({
-    targets: '#penner .InOutExpo', translateX: 250, offset: 0,
-    easing: 'easeInOutExpo'
-  })
-  .add({
-    targets: '#penner .InOutCirc', translateX: 250, offset: 0,
-    easing: 'easeInOutCirc'
-  })
-  .add({
-    targets: '#penner .InOutBack', translateX: 250, offset: 0,
-    easing: 'easeInOutBack'
-  });
+!function(r,n){"function"==typeof define&&define.amd?define(n):"object"==typeof exports?module.exports=n():r.transformicons=n()}(this||window,function(){"use strict";var r={},n={transform:["click"],revert:["click"]},t=function(r){return"string"==typeof r?Array.prototype.slice.call(document.querySelectorAll(r)):void 0===r||r instanceof Array?r:[r]},o=function(r){return"string"==typeof r?r.toLowerCase().split(" "):r},e=function(r,e,f){var i=(f?"remove":"add")+"EventListener",s=t(r),a=s.length,u={};for(var l in n)u[l]=e&&e[l]?o(e[l]):n[l];for(;a--;)for(var d in u)for(var m=u[d].length;m--;)s[a][i](u[d][m],c)},c=function(n){r.toggle(n.currentTarget)};return r.add=function(n,t){return e(n,t),r},r.remove=function(n,t){return e(n,t,!0),r},r.transform=function(n){return t(n).forEach(function(r){r.classList.add("tcon-transform")}),r},r.revert=function(n){return t(n).forEach(function(r){r.classList.remove("tcon-transform")}),r},r.toggle=function(n){return t(n).forEach(function(n){r[n.classList.contains("tcon-transform")?"revert":"transform"](n)}),r},r});
