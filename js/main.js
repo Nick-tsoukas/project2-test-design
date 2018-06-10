de543a896fcc9abdd73c647b1d50383278f3ae9f
Nikolaos Tsoukas <nikolaostsoukas@Nikolaoss-iMac.local>
2018-06-10 17:47:10 -0600

    Adding other pages

--- js/main.js
+++ js/main.js
@@ -4,17 +4,9 @@ const circleType = new CircleType(document.getElementById('demo1'));
 // Set the text radius and direction. Note: setter methods are chainable.
 circleType.radius(384);
 
-var demo2 = new CircleType(document.getElementById('demo2'));
-
-demo2.radius(384);
-
-// Provide your own splitter function to handle emojis
-// @see https://github.com/orling/grapheme-splitter
-// const splitter = new GraphemeSplitter()
-// new CircleType(
-//   document.getElementById('demo1'),
-//   splitter.splitGraphemes.bind(splitter)
-// );
+// var demo2 = new CircleType(document.getElementById('demo2'));
+//
+// demo2.radius(384);
 
 
 !function(r,n){"function"==typeof define&&define.amd?define(n):"object"==typeof exports?module.exports=n():r.transformicons=n()}(this||window,function(){"use strict";var r={},n={transform:["click"],revert:["click"]},t=function(r){return"string"==typeof r?Array.prototype.slice.call(document.querySelectorAll(r)):void 0===r||r instanceof Array?r:[r]},o=function(r){return"string"==typeof r?r.toLowerCase().split(" "):r},e=function(r,e,f){var i=(f?"remove":"add")+"EventListener",s=t(r),a=s.length,u={};for(var l in n)u[l]=e&&e[l]?o(e[l]):n[l];for(;a--;)for(var d in u)for(var m=u[d].length;m--;)s[a][i](u[d][m],c)},c=function(n){r.toggle(n.currentTarget)};return r.add=function(n,t){return e(n,t),r},r.remove=function(n,t){return e(n,t,!0),r},r.transform=function(n){return t(n).forEach(function(r){r.classList.add("tcon-transform")}),r},r.revert=function(n){return t(n).forEach(function(r){r.classList.remove("tcon-transform")}),r},r.toggle=function(n){return t(n).forEach(function(n){r[n.classList.contains("tcon-transform")?"revert":"transform"](n)}),r},r});
